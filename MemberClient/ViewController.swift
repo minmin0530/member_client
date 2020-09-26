4ae029a25b8da558e6a25435dcfa09f80c99010b
minmin0530 <izumiyoshiki@icloud.com>
2020-09-26 09:40:01 +0900

    first commit

---
+++ MemberClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemberClient
+//
+//  Created by Yoshiki Izumi on 2020/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
